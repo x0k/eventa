e98e6673bbe6c88fe986ac1e6b7b0ea2dd934ba0
Krasilnikov Roman <x0kill@ya.ru>
2019-05-28 07:40:49 +0300

    Update grouper module

--- src/grouper/index.ts
+++ src/grouper/index.ts
@@ -1,27 +1,35 @@
 import { reduceIterable } from 'iterator-wrapper'
 
-import { IConstraints, DATE_PARTS } from 'utils/schedule'
-import { IPeriod, IDictionary } from 'utils'
+import { IConstraints } from 'utils/schedule'
+import { IPeriod } from 'utils'
+
+import { IDateTime } from 'iterator'
 
 import * as H from 'utils/dateTime'
 
-function calcStepFromConstraints (constraints: IConstraints): number | false {
-  // @ts-ignore
-  const part = DATE_PARTS.find(part => Boolean(constraints[part]) && Boolean(constraints[part].step))
-  // @ts-ignore
-  return part && constraints[part].step * H[part]
-}
+import { calcStepFromConstraints } from './step'
 
-interface IGroupedEvent {
+export interface IGroupedEvent<T extends IDateTime> {
   period: IPeriod<number>
-  value: IDictionary<any>
+  value?: T
 }
 
-export function grouper<T> (iterable: IterableIterator<T>, constraints: IConstraints) {
-  const step = calcStepFromConstraints(constraints) || H.minute
-  
-  function separator (previous: IGroupedEvent, current: IDictionary<any>) {
-    return 
-  }
+type TSeparator<T extends IDateTime> = (step: number) => (previous: IGroupedEvent<T>, current: T) => boolean
 
+type TMerge<T extends IDateTime> = (previous: T, current: T) => T
+
+function reducer<T extends IDateTime> (merge: TMerge<T>) {
+  return ({ period: { start }, value }: IGroupedEvent<T>, current: T) => {
+    if (value === undefined) {
+      const { milliseconds: ms } = current
+      return { value: current, period: { start: ms, end: ms } }
+    }
+    const end: number = current.milliseconds
+    return { value: merge(value, current), period: { start, end } }
+  }
+}
+
+export function grouper<T extends IDateTime> (iterable: IterableIterator<T>, constraints: IConstraints, separator: TSeparator<T>, merge: TMerge<T>) {
+  const step = calcStepFromConstraints(constraints) || H.minute
+  return reduceIterable(iterable, separator(step), reducer(merge), { period: { start: 0, end: 0 } })
 }
---
+++ src/grouper/step.ts
@@ -0,0 +1,10 @@
+import { IConstraints, DATE_PARTS } from 'utils/schedule'
+
+import * as H from '../utils/dateTime'
+
+export function calcStepFromConstraints (constraints: IConstraints): number | false {
+  // @ts-ignore
+  const part = DATE_PARTS.find(part => Boolean(constraints[part]) && Boolean(constraints[part].step))
+  // @ts-ignore
+  return part && constraints[part].step * H[part]
+}

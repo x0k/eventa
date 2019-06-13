9faac6986149baaa46f77bb93e7c5cb0d7448818
Krasilnikov Roman <x0kill@ya.ru>
2019-06-13 20:19:40 +0300

    Optimize call rule

--- src/converter/__tests__/expressionBuilder.test.ts
+++ src/converter/__tests__/expressionBuilder.test.ts
@@ -1,25 +1,6 @@
-import { IRule } from '../../utils/schedule';
-
 import { IEvent } from '../utils'
 
 import { buildExpression } from '../buildExpression'
-
-const rules: IRule[] = [
-  {
-    id: 'call',
-    expression: [
-      "@case", ["$>>", "@dateTime", ["time"], "@in", [{ "start": -2208969017000, "end": -2208963617000 }], 1,
-        "@case", ["$>>", "@dateTime", ["time"], "@in", [{ "start": -2208963017000, "end": -2208957617000 }], 2,
-          "@case", ["$>>", "@dateTime", ["time"], "@in", [{ "start": -2208957017000, "end": -2208951617000 }], 3,
-            "@case", ["$>>", "@dateTime", ["time"], "@in", [{ "start": -2208948017000, "end": -2208942617000 }], 4,
-              "@case", ["$>>", "@dateTime", ["time"], "@in", [{ "start": -2208942017000, "end": -2208936617000 }], 5,
-                "@case", ["$>>", "@dateTime", ["time"], "@in", [{ "start": -2208936017000, "end": -2208930617000 }], 6,
-                  "@case", ["$>>", "@dateTime", ["time"], "@in", [{ "start": -2208930017000, "end": -2208924617000 }], 7,
-                    "@case", ["$>>", "@dateTime", ["time"], "@in", [{ "start": -2208924017000, "end": -2208918617000 }], 8,
-                      "@default", [false] ] ] ] ] ] ] ] ]
-    ]
-  }
-]
 
 const events: IEvent[] = [
   {
